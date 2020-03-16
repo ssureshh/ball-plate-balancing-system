a5efbdaedd0a3f18863d69ade7d2c5a39c131635
SureshKumar <suresh241098@gmail.com>
2020-03-16 22:02:54 +0530

    partially working

--- src/python/main.py
+++ src/python/main.py
@@ -1,15 +1,38 @@
 import camlib
 import cv2
-import pid
+import pidlib
 import serial_com as servos
 import time
 
 def main():
-    
+    pid_x = pidlib.PID_controller()
+    pid_y = pidlib.PID_controller()
+    cam = camlib.camera()
 
+    x_center = 414
+    y_center = 310
 
+    while 1:
+        cam.calc_currentPos()
+        error_x = x_center - cam.pos_x
+        error_y = y_center - cam.pos_y
+        #print("X_error : "+str(error_x)+"| Y error : "+str(error_y))
+        pid_x.compute_PID(error_x, "X_AXIS")
+        pid_y.compute_PID(error_y, "==Y_AXIS")
+        # print("X_output : "+str(pid_x.output)+"| Y output : "+str(pid_y.output))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
 
 if __name__ == "__main__":
     main()
+
+
+"""
+Notes : 
+
+print("xxx : "+str(123))
+print("xxx : "+str(123)+"| yyy : "+str(456))
+
+"""
--- src/python/pidlib.py
+++ src/python/pidlib.py
@@ -1,15 +1,15 @@
 import re
 import time
 
-class pid_controller(object):
+class PID_controller(object):
     def __init__(self, current_time=None):
         file = open("variables.conf","r")
         lines = file.readlines()
         kp=float(re.split(':',lines[7])[1])
         ki=float(re.split(':',lines[8])[1])
         kd=float(re.split(':',lines[9])[1])
-        out_min=re.split(':',lines[10])[1]
-        out_max=re.split(':',lines[11])[1]
+        out_min=float(re.split(':',lines[10])[1])
+        out_max=float(re.split(':',lines[11])[1])
         file.close()
         self.kp = kp
         self.ki = ki
@@ -20,19 +20,25 @@ class pid_controller(object):
         self.current_time = current_time if current_time is not None else time.time()
         self.previous_time = self.current_time
         self.last_error = 0.00
+        self.cumulative_error = 0.00
+        self.output = 0.00
 
-    def compute_pid(self, error, current_time=None):
+    def compute_PID(self,error,name,current_time=None):
+        #print("Error : "+str(error))
         self.current_time = current_time if current_time is not None else time.time()
         elasped_time = self.current_time - self.previous_time
+        #print("AXIS : "+str(name)+"| current_time : "+str(self.current_time)+"| elapsed_time : "+str(elasped_time)+"| previous_time : "+str(self.previous_time))
 
-        self.cumulative_error += error*self.elasped_time
-        self.rate_error = (error - self.last_error)/self.elasped_time
+        self.cumulative_error += error*elasped_time
+        self.rate_error = (error - self.last_error)/elasped_time
+        print(str(name)+" => "+"cumErr : "+str(self.cumulative_error)+" | rateErr : "+str(self.rate_error))
+
         if self.cumulative_error > 20:
             self.cumulative_error = 20
         if self.cumulative_error < -20:
             self.cumulative_error = -20
 
-        self.output = self.kp*error + self.ki*cumulative_error + self.kd*rate_error
+        self.output = self.kp*error + self.ki*self.cumulative_error + self.kd*self.rate_error
 
         self.last_error = error
         self.previous_time = self.current_time
